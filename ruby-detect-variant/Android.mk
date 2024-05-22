1a72e6f9b5e3c23e5cf3df65107ef481c4df0d8d
YZBruh <yagizzengin73@gmail.com>
2024-05-22 16:57:45 +0300

    Update Android.mk

--- ruby-detect-variant/Android.mk
+++ ruby-detect-variant/Android.mk
@@ -4,14 +4,17 @@ include $(CLEAR_VARS)
 
 # module info
 LOCAL_MODULE := ruby-detect-variant
-LOCAL_MODULE_CLASS := RECOVERY_EXECUTABLES
+LOCAL_MODULE_CLASS := EXECUTABLES
 LOCAL_MODULE_TAGS := optional
 
 # install in to /system/bin
 LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/system/bin
 
 # compiler flags
-LOCAL_CFLAGS = -O3 -Wall -g
+LOCAL_CFLAGS = \
+    -O3 \
+    -Wall \
+    -g
 
 # source
 LOCAL_SRC_FILES = \
