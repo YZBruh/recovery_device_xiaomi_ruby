b88bc50b1781f367a72b7f2ee48c449172e31ec3
Yağız Zengin <yagizzengin73@gmail.com>
2024-06-10 23:57:11 +0300

    ruby-detect-variant: Format Android.mk properly

--- ruby-detect-variant/Android.mk
+++ ruby-detect-variant/Android.mk
@@ -1,28 +1,37 @@
+# By YZBruh
+
+# Copyright (C) 2024 The Android Open Source Project
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-# module info
+# general ruby-detect-variant variables
 LOCAL_MODULE := ruby-detect-variant
 LOCAL_MODULE_CLASS := RECOVERY_EXECUTABLES
 LOCAL_MODULE_TAGS := optional
-
-# install in to /system/bin
-LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/system/bin
-
-# compiler flags
-LOCAL_CFLAGS = -O3 -Wall -g
-
-# source
 LOCAL_SRC_FILES = \
     ruby-detect-variant.c \
     write-recovery-log.c \
     load-variant.c
-
-# headers
+LOCAL_CFLAGS = \
+    -O3 \
+    -Wall \
+    -g
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/ruby
-
-# link liblog
 LOCAL_SHARED_LIBRARIES := liblog
+LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/system/bin
 
 include $(BUILD_EXECUTABLE)
