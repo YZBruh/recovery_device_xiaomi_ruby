dbc5437ba6ab3082e71a195b8d25fbb0115211ae
Yağız Zengin <yagizzengin73@gmail.com>
2024-06-12 09:39:03 +0300

    Set the file system of the system partition to ext4 in the A/B OTA configuration

--- device.mk
+++ device.mk
@@ -46,7 +46,7 @@ AB_OTA_PARTITIONS += \
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/mtk_plpath_utils \
-    FILESYSTEM_TYPE_system=erofs \
+    FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
 # Boot control HAL
